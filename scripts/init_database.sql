79b288c0c23e1da1f0a22484d9c0807efb1f1ec1
ritikkaliyar-ai <ritikkaliyar13@gmail.com>
2026-04-15 19:29:22 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,16 @@
+/*
+Create Database and Schemas
+
+Purpose: The script will create a database named "BrazilianSales" and also setup 3 schemas with the database i.e. Bronze, Silver and Gold.
+DBMS: PostgreSQL
+
+*/
+
+CREATE DATABASE BrazilianSales;
+
+CREATE SCHEMA Bronze;
+CREATE SCHEMA Silver;
+CREATE SCHEMA Gold;
+
+
+
